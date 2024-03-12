b38e1fc04d357a0c5ec52426187868de1a24aabd
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-12 14:39:18 -0300

    Fineshed the Header component

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/defaul'
 import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
-      <div>App</div>
+      <GlobalStyle />
+      <Transactions />
     </ThemeProvider>
   )
 }
