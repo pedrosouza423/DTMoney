9646b1f653812ec7ec8d229950be35c67b1767b1
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-16 11:36:02 -0300

    Added TransactionsTable

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 
 export const Transactions = () => {
   return (
     <div>
         <Header />
         <Summary />
+
+        <TransactionsContainer>
+          <TransactionsTable>
+            <tbody>
+              <tr>
+                <td width="50%">Desenvolvimento de website</td>
+                <td>
+                  <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
+                </td>
+                <td>Venda</td>
+                <td>15/03/2021</td>
+              </tr>
+              <tr>
+                <td width="50%">Hamburguer</td>
+                <td>
+                  <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+                </td>
+                <td>Alimentação</td>
+                <td>17/03/2021</td>
+              </tr>
+            </tbody>
+          </TransactionsTable>
+        </TransactionsContainer>
     </div>
   )
 }
