d3cd0cda0d811f453b54bf501c3b7b961e8e5305
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-22 18:47:49 -0300

    Created a Context Transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/defaul'
 import { GlobalStyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
+import { TransactionProvider } from './contexts/TransactionContext'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+      <TransactionProvider>
+       <Transactions />
+      </TransactionProvider>
     </ThemeProvider>
   )
 }
